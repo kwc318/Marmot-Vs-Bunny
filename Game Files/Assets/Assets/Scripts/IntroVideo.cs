1d9ba5c07f39abca50ba4c4a51bd86cab2b39ad2
kwc318 <kwc318@nyu.edu>
2020-04-20 03:41:33 +0800

    4/20/2020 - Built intro video

---
+++ Game Files/Assets/Assets/Scripts/IntroVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroVideo : MonoBehaviour
+{
+    public GameObject videoplayer;
+    public float stoptime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stoptime -= Time.deltaTime;
+		if (stoptime <= 0)
+		{
+			SceneManager.LoadScene("SampleScene");
+		}
+    }
+}
