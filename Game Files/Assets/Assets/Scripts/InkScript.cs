c40bcc3ad898f22b659886288511eeb33de023aa
kwc318 <kwc318@nyu.edu>
2020-05-18 04:58:22 +0800

    Changes as of 5/18/2020

--- Game Files/Assets/Assets/Scripts/InkScript.cs
+++ Game Files/Assets/Assets/Scripts/InkScript.cs
@@ -1,22 +1,34 @@
 ﻿using Ink.Runtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InkScript : MonoBehaviour
 {
     public TextAsset inkJSON;
     private Story story;
+    public TextMeshProUGUI textPrefab;
+    public Button buttonPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         story = new Story(inkJSON.text);
+
+        TextMeshProUGUI storyText = Instantiate(textPrefab) as TextMeshProUGUI;
+        storyText.text = loadStoryChunk();
+        storyText.transform.SetParent(this.transform, false);
+
         Debug.Log(loadStoryChunk());
         
-        for(int i = 0; i <story.currentChoices.Count; i++)
+        foreach (Choice choice in story.currentChoices)
         {
-            Debug.Log(story.currentChoices[i].text);
+            Button choiceButton = Instantiate(buttonPrefab) as Button;
+            TextMeshProUGUI choiceText = choiceButton.GetComponentInChildren<TextMeshProUGUI>();
+            choiceText.text = choice.text;
+            choiceButton.transform.SetParent(this.transform, false);
         }
 
         story.ChooseChoiceIndex(2);
